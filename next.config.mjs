99986461055f624ab584840a3862709c51e5a425
Your Name <you@example.com>
2025-02-16 14:12:20 +0100

    primero

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            'wznempxaasdpiy6c.public.blob.vercel-storage.com',
+            's.gravatar.com',
+            'lh3.googleusercontent.com'
+        ]
+    }
+};
+
+export default nextConfig;
